5723c77de7666413c4e4fe5935691a926b48793a
Azim4ik_06 <saidmurodovazim808@gmail.com>
2024-04-28 11:59:35 +0500

    исправление ошибок

--- js/navbar.js
+++ js/navbar.js
@@ -2,15 +2,24 @@ const navbarToggler = document.querySelector('.navbar-toggler');
 const menu = document.querySelector('#navbarMenu');
 const overlay = document.querySelector('.overlay');
 const closeNavbar = document.querySelector('.btn-X');
+const btnDown = document.querySelector('.btn-down');
+const backImg = document.querySelector('.back-img');
+const learning = document.querySelector('.learning')
 
 navbarToggler.addEventListener('click', navbarShow)
 function navbarShow() {
-    menu.classList.toggle('d-none')
-    overlay.classList.toggle('d-none')
+    menu.classList.toggle('d-none');
+    overlay.classList.toggle('d-none');
+    btnDown.classList.toggle('d-none');
+    backImg.classList.toggle('d-none');
+    learning.classList.toggle('d-none');
 }
 
 closeNavbar.addEventListener('click', closeNav);
 function closeNav() {
-    menu.classList.toggle('d-none')
-    overlay.classList.toggle('d-none')
+    menu.classList.toggle('d-none');
+    overlay.classList.toggle('d-none');
+    btnDown.classList.toggle('d-none');
+    backImg.classList.toggle('d-none');
+    learning.classList.toggle('d-none');
 }
